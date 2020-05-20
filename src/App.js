5ac9d7f6014038320d0d6db420fa88dfbb10bb7a
judrin <ingonlee7942@gmail.com>
2020-05-19 23:24:45 -0400

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import GlobalStyled from './global';
+import Nav from './Nav';
 import Board from './Board';
+import SavedItems from './SavedItems';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyled />
-      <Board />
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Board} />
+          <Route path="/saved" component={SavedItems} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
